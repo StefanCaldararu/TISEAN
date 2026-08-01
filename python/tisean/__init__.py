1a9294c350e28d502dd4dbd42a9fa000aebb248a
TAEP Engineer <taep-engineer@localhost>
2026-08-01 22:23:41 +0000

    Fix #39: wrap av-d2 code
    Generated by the TAEP Engineer worker.

--- python/tisean/__init__.py
+++ python/tisean/__init__.py
@@ -1,4 +1,4 @@
 from . import _tisean
-from ._tisean import ar_model, low121, histogram, polypar, corr, xcor
+from ._tisean import ar_model, low121, histogram, polypar, corr, xcor, av_d2
 
-__all__ = ["ar_model", "low121", "histogram", "polypar", "corr", "xcor"]
+__all__ = ["ar_model", "low121", "histogram", "polypar", "corr", "xcor", "av_d2"]
