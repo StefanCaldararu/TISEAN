7278983bdc9ee38f0695fd6bf8625e923a498325
StefanCaldararu <52421529+StefanCaldararu@users.noreply.github.com>
2026-08-05 23:20:17 -0500

    Fix #54: makenoise python bindings (#55)
    Generated by the TAEP Engineer worker.
    
    Co-authored-by: TAEP Engineer <taep-engineer@localhost>

--- python/tisean/__init__.py
+++ python/tisean/__init__.py
@@ -1,4 +1,4 @@
 from . import _tisean
-from ._tisean import ar_model, low121, histogram, polypar, corr, xcor, av_d2, mutual, extrema, recurr, xzero, mem_spec, lyap_r
+from ._tisean import ar_model, low121, histogram, polypar, corr, xcor, av_d2, mutual, extrema, recurr, xzero, mem_spec, lyap_r, makenoise
 
-__all__ = ["ar_model", "low121", "histogram", "polypar", "corr", "xcor", "av_d2", "mutual", "extrema", "recurr", "xzero", "mem_spec", "lyap_r"]
+__all__ = ["ar_model", "low121", "histogram", "polypar", "corr", "xcor", "av_d2", "mutual", "extrema", "recurr", "xzero", "mem_spec", "lyap_r", "makenoise"]
